398438171a35116009c3a168cbb291e357745788
nassir <nassirmz@gmail.com>
2016-07-05 16:44:05 -0700

    (feat) Add support for CHANGE_NAME action

--- app/app.jsx
+++ app/app.jsx
@@ -17,4 +17,4 @@ ReactDOM.render(
   document.getElementById('app')
 );
 
-require('./redux-todo.jsx');
+require('./redux-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,10 +1,30 @@
 var redux = require('redux');
 
 var reducer = (state = { name: 'Anonymous' }, action) => {
-  return state;
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+    return {
+      ...state,
+      name: action.name
+    };
+    default:
+    return state;
+  }
 }
 
 var store = redux = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('current State', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Nassir'
+};
+
+store.dispatch(action);
+
+console.log('Name should be andrew', store.getState());
+
+//only requirement of our actions are their type property
